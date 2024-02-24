4dc443b11f93b6824a2f1be78e8f96419aa2c649
Ech0n <43513980+Ech0n@users.noreply.github.com>
2024-02-24 01:23:10 +0100

    Extracted server code to BlefServer class Added test for creating a game

---
+++ src/BlefServer.ts
@@ -0,0 +1,37 @@
+import { Socket, Server } from 'socket.io';
+import { io } from 'socket.io-client';
+import http from 'http';
+import { config } from '../config';
+import { socketApi, SessionSocket } from './socketServer';
+import session from 'express-session';
+
+export class BlefServer {
+    io: Server;
+    serverSocket?: Socket;
+    rooms = new Set<string>();
+    roomHosts = new Map<string, string>();
+
+    constructor(server: http.Server) {
+        this.io = new Server(server, {
+            cors: {
+                origin: ['http://localhost:5173', 'http://localhost:5174', config.FRONTEND_SERVER_ADDRESS],
+                methods: ['GET', 'POST'],
+                allowedHeaders: ['my-custom-header'],
+                credentials: true,
+            },
+        });
+        const sessionConfig = session({
+            resave: true,
+            saveUninitialized: true,
+            secret: config.sessionSecret,
+            rolling: false,
+        });
+
+        this.io.engine.use(sessionConfig);
+
+        this.io.on('connection', (socket) => {
+            this.serverSocket = socket;
+            socketApi(this, <SessionSocket>socket);
+        });
+    }
+}
