2f15bbec0e78183ac5994ac6d6e0c5b08477bfe2
Ech0n <43513980+Ech0n@users.noreply.github.com>
2024-03-22 20:58:26 +0100

    config update

--- config.ts
+++ config.ts
@@ -4,7 +4,8 @@ const front_port = 5173;
 export const config = {
     BACKENDSERVERPORT: 5678,
     FRONTENDSERVERPORT: 5173,
-    BACKEND_SERVER_ADDRESS: 'localhost:' + back_port,
-    FRONTEND_SERVER_ADDRESS: 'localhost:' + front_port,
+    HOSTNAME: '10.244.172.231',
+    BACKEND_SERVER_ADDRESS: 'http://10.244.172.231:' + back_port,
+    FRONTEND_SERVER_ADDRESS: 'http://10.244.172.231:' + front_port,
     sessionSecret: 'pizda', // 0:
 };
--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,12 @@ import { checkToPlayersPayload, checkToServerPayload, gameStartPayload, hitPaylo
 import { config } from '../config';
 import { BlefServer } from './BlefServer';
 const port = config.BACKENDSERVERPORT || 5678;
+const hostname = config.HOSTNAME;
 
 // TODO: Reconsider using sessionId as identifier. Pro: this would remove need for usernames map since one could just get the data from session stroage. Con: Security?
 
-const server = http.createServer(app).listen(port, () => {
-    console.log(`Server is up at port http://localhost:${port}`);
+const server = http.createServer(app).listen(port, hostname, () => {
+    console.log(`Server is up at port http://{}:${port}`);
 });
 const sessionConfig = session({
     resave: true,
