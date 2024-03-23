f72962ed0e174c79eb98085f73b63707c5a7e0ee
Ech0n <43513980+Ech0n@users.noreply.github.com>
2024-03-23 20:04:28 +0100

    config changes before deployment tests

--- config.ts
+++ config.ts
@@ -4,7 +4,7 @@ const front_port = 5173;
 export const config = {
     BACKENDSERVERPORT: 5678,
     FRONTENDSERVERPORT: 5173,
-    hostname: '57.128.199.162',
+    hostname: 'localhost',
     BACKEND_SERVER_ADDRESS: 'localhost:' + back_port,
     FRONTEND_SERVER_ADDRESS: 'localhost:' + front_port,
 };
