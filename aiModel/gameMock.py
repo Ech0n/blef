bffbd6bcdd5b9ac8e738a329778832e61d268713
Mateusz <matid@spoko.pl>
2025-05-07 23:35:44 +0200

    Deleted not used aiModel dir

--- aiModel/gameMock.py
+++
@@ -1,3 +0,0 @@
-cardHierarchy = ['2','3','4','5','6','7','8','9','10','j','q','k','a' ]
-colorHierarchy = ['spade','heart','club','diamond']
-movesHierarchy = ['royal','flush','color','four','full','street','three','double','pair','one']
--- aiModel/gameRunner.py
+++
@@ -1,7 +0,0 @@
-import js2py 
-  
-
-js2py.translate_file('compiledGame/CompiledGame.js', 'game.py')
-
-from game import *
-
